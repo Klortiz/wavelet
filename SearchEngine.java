777bad1247497d10fe303a6b3fe4bceb12b74a9f
Klortiz <klortiz@ucsd.edu>
2023-01-19 09:31:18 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,13 +2,13 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    String[] words;
+    String word;
     int count = -1;
     int num = 0;
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Kristhian's Number: %d", num);
+            return String.format("Kristhian's Number: %d \nKristhian's Word: %s", num, word);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
@@ -20,12 +20,9 @@ class Handler implements URLHandler {
                     num += Integer.parseInt(parameters[1]);
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
                 }
-            }
-            else if(url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
                 if(parameters[0].equals("word")) {
-                    words[0] = parameters[1];
-                    return String.format("Your word is: %s", parameters[1], words);
+                    word = parameters[1];
+                    return String.format("Your word is: %s", parameters[1], word);
                 }
             }
             return "404 Not Found!";
